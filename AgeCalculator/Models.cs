4705e3e03d396d568e3afc626e6aa48a35be82c9
Faruk ul Islam <faruk.ice.pust@gmail.com>
2022-12-06 13:00:03 +0600

    age calculator updating

---
+++ AgeCalculator/Models.cs
@@ -0,0 +1,15 @@
+﻿namespace AgeCalculator
+{
+    public class ModelsProperties
+            {
+        public string name { get; set; }
+        public  DateTime Boxdate{ get; set; }
+     public   DateTime Time = DateTime.Now;
+       //public TimeSpan age = time.Subtract(Boxdate);
+
+    
+   // string time = DateTime.Now.ToString("h:mm:ss tt");
+  
+    
+}
+}
